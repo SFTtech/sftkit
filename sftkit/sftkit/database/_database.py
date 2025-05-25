290e66357081283753955ceabcc36bab6d71617c
Michael Loipführer <michael.loipfuehrer@stusta.de>
2025-05-25 15:19:05 +0200

    fix: allow blacklisting db functions to prevent being dropped

--- sftkit/sftkit/database/_database.py
+++ sftkit/sftkit/database/_database.py
@@ -18,7 +18,12 @@ class Database:
         self._pool = await create_db_pool(self.config, n_connections=n_connections)
         return self._pool
 
-    async def apply_migrations(self, until_migration: str | None = None) -> None:
+    async def apply_migrations(
+        self,
+        until_migration: str | None = None,
+        function_blacklist: list[str] | None = None,
+        function_blacklist_prefix: str | None = None,
+    ) -> None:
         pool = await self.create_pool(n_connections=1)
         try:
             await apply_migrations(
@@ -26,6 +31,8 @@ class Database:
                 migration_path=self.migrations_dir,
                 code_path=self.code_dir,
                 until_migration=until_migration,
+                function_blacklist=function_blacklist,
+                function_blacklist_prefix=function_blacklist_prefix,
             )
         finally:
             await pool.close()
@@ -33,13 +40,22 @@ class Database:
     async def attach(self):
         return await psql_attach(self.config)
 
-    async def reload_code(self):
+    async def reload_code(
+        self,
+        function_blacklist: list[str] | None = None,
+        function_blacklist_prefix: str | None = None,
+    ):
         pool = await self.create_pool(n_connections=1)
 
         try:
             async with pool.acquire() as conn:
                 async with conn.transaction(isolation="serializable"):
-                    await reload_db_code(conn, code_path=self.code_dir)
+                    await reload_db_code(
+                        conn,
+                        code_path=self.code_dir,
+                        function_blacklist=function_blacklist,
+                        function_blacklist_prefix=function_blacklist_prefix,
+                    )
         finally:
             await pool.close()
 
